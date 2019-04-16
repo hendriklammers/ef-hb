d7452521bd2988f164b205413316666b1ea85b79
Hendrik Lammers <lammers@gmail.com>
2019-04-16 21:37:17 +0700

    Add scotty

--- src/Main.hs
+++ src/Main.hs
@@ -1,5 +1,18 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module Main where
 
+import Data.Maybe (fromMaybe)
+import System.Environment (lookupEnv)
+import Text.Read (readMaybe)
+import Web.Scotty
+
+routes :: ScottyM ()
+routes = do
+    get "/" $ file "client/index.html"
+    notFound $ file "404.html"
+
 main :: IO ()
-main =
-  putStrLn "hello world"
+main = do
+    port <- fromMaybe 3000 . (readMaybe =<<) <$> lookupEnv "PORT"
+    scotty port routes
