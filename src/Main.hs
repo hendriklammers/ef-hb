e714c9e33a7278f47688b6d95483b9e83830fc31
Hendrik Lammers <lammers@gmail.com>
2019-04-16 22:18:38 +0700

    Serve index from static dir

--- src/Main.hs
+++ src/Main.hs
@@ -3,16 +3,20 @@
 module Main where
 
 import Data.Maybe (fromMaybe)
+import Network.Wai.Middleware.RequestLogger
+import Network.Wai.Middleware.Static
 import System.Environment (lookupEnv)
 import Text.Read (readMaybe)
 import Web.Scotty
 
 routes :: ScottyM ()
 routes = do
-    get "/" $ file "client/index.html"
-    notFound $ file "404.html"
+    get "/" $ file "./static/index.html"
 
 main :: IO ()
 main = do
     port <- fromMaybe 3000 . (readMaybe =<<) <$> lookupEnv "PORT"
-    scotty port routes
+    scotty port $ do
+        middleware logStdoutDev
+        middleware $ staticPolicy (noDots >-> addBase "static")
+        routes
