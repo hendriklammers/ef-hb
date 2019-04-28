7819fd1a4fa2e1a98def14ece50b2e58c8e16b14
Hendrik Lammers <lammers@gmail.com>
2019-04-28 11:40:36 +0700

    Load users from api

--- client/src/Main.elm
+++ client/src/Main.elm
@@ -1,45 +1,91 @@
 module Main exposing (main)
 
 import Browser
-import Html exposing (Html, text, h1)
+import Html exposing (Html, table, td, text, th, tr)
+import Http
+import Json.Decode as Decode exposing (Decoder)
+
+
+type alias User =
+    { id : Int
+    , name : String
+    , email : String
+    }
+
+
+userDecoder : Decoder User
+userDecoder =
+    Decode.map3 User
+        (Decode.field "id" Decode.int)
+        (Decode.field "name" Decode.string)
+        (Decode.field "email" Decode.string)
+
+
+usersDecoder : Decoder (List User)
+usersDecoder =
+    Decode.list userDecoder
 
 
 type alias Model =
-    String
+    List User
 
 
 type Msg
-    = NoOp
+    = ReceiveUsers (Result Http.Error (List User))
 
 
 init : () -> ( Model, Cmd Msg )
 init _ =
-    ( "Hello world", Cmd.none )
-
-
-subscriptions : Model -> Sub Msg
-subscriptions model =
-    Sub.none
+    ( []
+    , Http.get
+        { url = "/api/users"
+        , expect = Http.expectJson ReceiveUsers usersDecoder
+        }
+    )
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
     case msg of
-        NoOp ->
+        ReceiveUsers (Ok users) ->
+            ( users, Cmd.none )
+
+        _ ->
             ( model, Cmd.none )
 
 
 view : Model -> Html Msg
-view model =
-    h1 []
-        [text model]
+view users =
+    table []
+        (tr []
+            [ th []
+                [ text "id" ]
+            , th []
+                [ text "name" ]
+            , th []
+                [ text "email" ]
+            ]
+            :: List.map viewUser users
+        )
+
+
+viewUser : User -> Html Msg
+viewUser { id, name, email } =
+    tr []
+        [ td []
+            [ text (String.fromInt id) ]
+        , td []
+            [ text name ]
+        , td []
+            [ text email ]
+        ]
 
 
 main : Program () Model Msg
 main =
     Browser.element
         { init = init
-        , subscriptions = subscriptions
+        , subscriptions = always Sub.none
         , update = update
         , view = view
         }
